4f2ae692e693d3607a21e15a362de4d6bc2eb9f6
Zoriana Litovchenko <zlitovchenko@yandex.ru>
2022-12-03 00:44:37 +0300

    проектная работа 4

--- scripts/index.js
+++ scripts/index.js
@@ -4,25 +4,29 @@ const popUp = document.querySelector('.popup')
 let formElement = document.querySelector('.popup__form')
 let nameProfileInfo = document.querySelector('.profile__name');
 let jobProfileInfo = document.querySelector('.profile__profession');
-let nameInput = document.querySelector('#popup__field_name');
-let jobInput = document.querySelector('#popup__field_profession');
+let nameInput = document.querySelector('.popup__field_name');
+let jobInput = document.querySelector('.popup__field_profession');
+
 
 openPopUp.addEventListener('click', function(){
   popUp.classList.add('popup_open');
+  nameInput.value = nameProfileInfo.textContent;
+  jobInput.value = jobProfileInfo.textContent;
 });
 
 closePopUp.addEventListener('click', function(){
   popUp.classList.remove('popup_open');
 });
 
-formElement.addEventListener('submit', function(evt){
-    evt.preventDefault();
-    nameInput.value = nameProfileInfo.textContent;
-    jobInput.value = jobProfileInfo.textContent;
-    nameProfileInfo.textContent = nameInput.value;
-    jobProfileInfo.textContent = jobInput.value;
-    closePopUp();
-});
+function formSubmit (evt) {
+  evt.preventDefault();
+  nameProfileInfo.textContent = nameInput.value;
+  jobProfileInfo.textContent = jobInput.value;
+  popUp.classList.remove('popup_open');
+};
+
+formElement.addEventListener ('submit', formSubmit);
+
 
 
 

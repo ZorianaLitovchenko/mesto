8e43b4cf244150e6c2cdb6c480f1b712c1a2700a
Zoriana Litovchenko <zlitovchenko@yandex.ru>
2022-12-03 22:40:20 +0300

    j

--- blocks/scripts/index.js
+++ blocks/scripts/index.js
@@ -7,21 +7,23 @@ let jobProfileInfo = document.querySelector('.profile__profession');
 let nameInput = document.querySelector('.popup__field_name');
 let jobInput = document.querySelector('.popup__field_profession');
 
+
 openPopUp.addEventListener('click', function(){
   popUp.classList.add('popup_open');
   nameInput.value = nameProfileInfo.textContent;
   jobInput.value = jobProfileInfo.textContent;
 });
 
-closePopUp.addEventListener('click', function(){
-  popUp.classList.remove('popup_open');
-});
+function popUpClose() {
+  popup.classList.remove('popup_open');
+}
+closePopUp.addEventListener ('click', popUpClose);
 
 function formSubmit (evt) {
   evt.preventDefault();
   nameProfileInfo.textContent = nameInput.value;
   jobProfileInfo.textContent = jobInput.value;
-  closePopUp();
+  popUpClose()
 };
 
 formElement.addEventListener ('submit', formSubmit);
